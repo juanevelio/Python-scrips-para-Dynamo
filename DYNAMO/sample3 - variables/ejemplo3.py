93c4675e73379cd5105272323d1620c1e79c2fcd
juan molinas <juanevelio@gmail.com>
2019-02-26 13:39:19 -0300

    segungo commit

--- DYNAMO/sample3 - variables/ejemplo3.py
+++ DYNAMO/sample3 - variables/ejemplo3.py
@@ -2,9 +2,14 @@
 import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
- 
-x1 = IN[0]
-x2 = IN[1]
-x3 = IN[2]
-punto1 = Point.ByCoordinates(x1,x2,x3)
-OUT = punto1
+ #rectangulo
+altura= IN[0]
+anchura = IN[1]
+area = altura*anchura
+perimetro= 2*anchura+2*altura
+salida=""
+if (altura>0 and anchura>0):
+    salida = 'El area es  {} y el perimetro es {}'.format(area,perimetro)
+else:
+    salida = "Los valores deben ser reales positivos distintos de Cero"
+OUT= salida
